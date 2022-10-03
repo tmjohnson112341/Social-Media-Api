97e14e6c846aab3d70fdda49f868f55381d1edaa
Tina Johnson <tmjohnson112341@gmail.com>
2022-10-03 15:53:24 -0400

    exceptions

---
+++ src/main/java/com/cooksys/socialmedia/repositories/HashTagRepository.java
@@ -0,0 +1,8 @@
+package com.cooksys.socialmedia.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HashTagRepository {
+
+}
---
+++ src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
@@ -0,0 +1,5 @@
+package com.cooksys.socialmedia.repositories;
+
+public interface UserRepository {
+
+}

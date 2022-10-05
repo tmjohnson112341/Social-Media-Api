a9570e604b785264039caa3070cd32aea22fec08
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-10-05 09:47:32 -0500

    Update ContextDto.java

--- src/main/java/com/cooksys/socialmedia/dtos/ContextDto.java
+++ src/main/java/com/cooksys/socialmedia/dtos/ContextDto.java
@@ -2,6 +2,8 @@ package com.cooksys.socialmedia.dtos;
 
 import java.util.List;
 
+@NoArgsConstructor
+@Data
 public class ContextDto {
     private TweetResponseDto target;
     private List<TweetResponseDto> before;

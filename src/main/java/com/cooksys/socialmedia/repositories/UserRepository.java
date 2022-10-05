7eada76d3371363a2c6d841b020bcf2e8cfb8dc4
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-10-05 09:59:48 -0500

    Update UserRepository.java

--- src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
+++ src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cooksys.socialmedia.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }

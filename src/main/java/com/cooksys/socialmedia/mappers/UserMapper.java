f430e9389a711ef67c9e45465daf1d9d50f9d5ea
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-10-05 09:58:15 -0500

    Update UserMapper.java

--- src/main/java/com/cooksys/socialmedia/mappers/UserMapper.java
+++ src/main/java/com/cooksys/socialmedia/mappers/UserMapper.java
@@ -4,9 +4,12 @@ import com.cooksys.socialmedia.dtos.UserRequestDto;
 import com.cooksys.socialmedia.dtos.UserResponseDto;
 import com.cooksys.socialmedia.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { ProfileMapper.class, CredentialsMapper.class })
 public interface UserMapper {
+    
+    @Mapping(target = "username", source = "credentials.username")
     UserResponseDto entityToDto (User entity);
 
     User dtoToEntity (UserRequestDto userRequestDto);

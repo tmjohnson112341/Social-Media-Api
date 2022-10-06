8e46e8426e181a67ca79410d4cdb93883f876fde
Tina Johnson <tmjohnson112341@gmail.com>
2022-10-06 11:58:47 -0400

    validateTag endpoint completed

--- src/main/java/com/cooksys/socialmedia/services/impl/ValidateServicesImpl.java
+++ src/main/java/com/cooksys/socialmedia/services/impl/ValidateServicesImpl.java
@@ -1,17 +1,31 @@
 package com.cooksys.socialmedia.services.impl;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.cooksys.socialmedia.entities.Hashtag;
+import com.cooksys.socialmedia.repositories.HashTagRepository;
+import com.cooksys.socialmedia.repositories.UserRepository;
 import com.cooksys.socialmedia.services.ValidateServices;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class ValidateServicesImpl implements ValidateServices {@Override
+public class ValidateServicesImpl implements ValidateServices {
+	
+	private final HashTagRepository hashtagRepository;
+	private final UserRepository userRepository;
+	
+	@Override
 	public boolean validateTag(String label) {
-		// TODO Auto-generated method stub
-		return false;
+		Optional<Hashtag>optionalHashtag = hashtagRepository.findByLabel(label);
+		if(optionalHashtag.isEmpty()) {
+			return false;
+		}else {
+			return true;
+		}	
 	}
 
 	@Override
